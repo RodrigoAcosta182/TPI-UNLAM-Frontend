777af80d45237d15fbaa09bed01eec34ec4a66e6
julioRodrigoAcosta <jracosta1991@gmail.com>
2022-10-10 17:51:58 -0300

    modal avatar

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,18 @@
 import { useContext, useEffect } from "react";
 import LoginBox from "../../components/auth/LoginBox/LoginBox";
 import Loading from "../../components/genericos/Loading/Loading";
+import ModalAvatar from "../../components/genericos/ModalAvatar/ModalAvatar";
 import { resetListError } from "../../context/action/listError/listError";
 import { GlobalContext } from "../../context/Provider";
 
 import "./Login.css";
 
 const Login = () => {
-  const { listErrorDispatch, authState } = useContext(GlobalContext);
+  const {
+    listErrorDispatch,
+    authState,
+    modalAvatarState,
+  } = useContext(GlobalContext);
 
   useEffect(() => {
     resetListError()(listErrorDispatch);
@@ -16,6 +21,7 @@ const Login = () => {
   return (
     <>
       <Loading state={authState.auth.loading} mensaje={"Accediendo"} />
+      {modalAvatarState.modalAvatar.show && <ModalAvatar />}
       <div className="login-container">
         <LoginBox></LoginBox>
       </div>
